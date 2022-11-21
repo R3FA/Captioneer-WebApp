d0da7533fb4c34d8039d27a4e3c501d4d116083a
Faris Skopak <77499895+R3FA@users.noreply.github.com>
2022-11-21 19:35:51 +0100

    AB#14 (#8)

---
+++ Captioneer.UI/src/app/Components/Main-Footer/footer-component/footer-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-component',
+  templateUrl: './footer-component.component.html',
+  styleUrls: ['./footer-component.component.css']
+})
+export class FooterComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Captioneer.UI/src/app/Components/Page-LogIn/page-login/page-login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-page-login',
+  templateUrl: './page-login.component.html',
+  styleUrls: ['./page-login.component.css']
+})
+export class PageLoginComponent implements OnInit {
+
+  sakrij: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+  }
+
+  loginForma: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
+
+  onLogin() {
+    if (!this.loginForma.valid) {
+      return;
+    }
+    console.log(this.loginForma.value);
+  }
+
+}
