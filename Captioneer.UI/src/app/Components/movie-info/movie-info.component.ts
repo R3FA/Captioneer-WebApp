ab2fe054c6497b688c2d876504f0b96a2721d6bb
BerunBiH <berun.agic@hotmail.com>
2023-01-08 03:04:44 +0100

    AB#65
    Implemented Google Maps into my project.

--- Captioneer.UI/src/app/Components/movie-info/movie-info.component.ts
+++ Captioneer.UI/src/app/Components/movie-info/movie-info.component.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { ActorMovies } from 'src/app/models/actor-movies';
 import { Observable } from 'rxjs';
 import { ThisReceiver } from '@angular/compiler';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-movie-info',
@@ -16,9 +17,33 @@ export class MovieInfoComponent implements OnInit {
   movie:any;
   movieObject:any;
   actors!:any;
+  mapPlaced!:google.maps.Map
   private url: string = environment.apiURL + "/ActorMovies";
 
   ngOnInit(): void {
+    let loader=new Loader({
+      apiKey:'AIzaSyCF4XwMfvYr_nME5E_nBbl9WgNqzfk6dLM'
+    })
+    loader.load().then(()=>{
+      this.mapPlaced=new google.maps.Map(document.getElementById("map")!,{
+        center:{lat: 34.098907, lng: -118.327759},
+        zoom: 6,
+      })
+      let marker= new google.maps.Marker({
+        position:{lat: 34.098907, lng: -118.327759},
+        map:this.mapPlaced,
+        draggable:true,
+      })
+
+      this.mapPlaced.addListener("click", (mapsMouseEvent:any) => {
+        // Create a new InfoWindow.
+          marker.setPosition(mapsMouseEvent.latLng);
+      })
+      marker.addListener("click", (mapsMouseEvent:any) => {
+        marker.setPosition();
+      })
+      
+    })
     let newObject = window.localStorage.getItem("selected movie");
     this.movie=newObject;
     this.movieObject=JSON.parse(this.movie);
